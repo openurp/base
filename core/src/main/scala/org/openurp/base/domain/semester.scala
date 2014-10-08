f3c472047e6d3b5a1753b76c52ce7cdd89ca67e0
xinzhou <xin.2165@gmail.com>
2014-10-08 15:55:29 +0800

    modify "IntIdBean" in semester

--- core/src/main/scala/org/openurp/base/domain/semester.scala
+++ core/src/main/scala/org/openurp/base/domain/semester.scala
@@ -1,16 +1,19 @@
 package org.openurp.base.domain
 
-import java.{ util => ju }
-
-import org.beangle.data.model.bean.{ CodedBean, NamedBean, NumIdBean, TemporalOnBean }
-import org.openurp.base.{ Calendar, Semester }
+import org.beangle.data.model.bean.CodedBean
+import org.beangle.data.model.bean.IntIdBean
+import org.beangle.data.model.bean.NamedBean
+import org.beangle.data.model.bean.NumIdBean
+import org.beangle.data.model.bean.TemporalOnBean
+import org.openurp.base.Calendar
+import org.openurp.base.Semester
 import org.openurp.base.WeekDay.WeekDay
 
 /**
  * 教学日历方案
  * 校历（日历方案）记录了一整套学年学期的设置，是连贯性学年学期设置的集合，也可称日历方案。
  */
-class CalendarBean extends NumIdBean[Int] with CodedBean with NamedBean with TemporalOnBean with Calendar {
+class CalendarBean extends IntIdBean with CodedBean with NamedBean with TemporalOnBean with Calendar {
   var semesters: Seq[Semester] = new collection.mutable.ListBuffer[Semester]
   var firstDay: WeekDay = _
 }
@@ -18,7 +21,7 @@ class CalendarBean extends NumIdBean[Int] with CodedBean with NamedBean with Tem
 /**
  * 学年学期 </p> 代表的是具体学年度的 学期设置，每个学期的起始日期（起始日期要以星期日作为第一天）和结束日期。
  */
-class SemesterBean extends NumIdBean[Int] with CodedBean with NamedBean with TemporalOnBean with Semester {
+class SemesterBean extends IntIdBean with CodedBean with NamedBean with TemporalOnBean with Semester {
   /**日历*/
   var calendar: Calendar = _
   /**学年度,一般为yyyy-yyyy或者yyyy的格式*/
