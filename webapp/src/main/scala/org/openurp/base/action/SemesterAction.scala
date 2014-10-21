2f79d0b67691c1bf1c109582929144743cc61f17
xinzhou <xin.2165@gmail.com>
2014-10-21 12:27:48 +0800

    modify calenderAction

--- webapp/src/main/scala/org/openurp/base/action/SemesterAction.scala
+++ webapp/src/main/scala/org/openurp/base/action/SemesterAction.scala
@@ -4,13 +4,18 @@ import org.beangle.commons.lang.Numbers
 import org.beangle.data.jpa.dao.OqlBuilder
 import org.beangle.webmvc.api.view.View
 import org.beangle.webmvc.entity.action.RestfulAction
-import org.openurp.base.Calendar
-import org.openurp.base.Semester
-import org.openurp.base.WeekDay
-import org.openurp.base.domain.CalendarBean
-import org.openurp.base.domain.SemesterBean
+import org.openurp.base.{Calendar, Semester, WeekDay}
+import org.openurp.base.domain.{CalendarBean, SemesterBean}
 
 class CalendarAction extends RestfulAction[Calendar]{
+  
+  override def editSetting(entity:Calendar){
+    val query = OqlBuilder.from(classOf[Semester])
+    query.orderBy("name")
+    val semesters = entityDao.search(query)
+    put("semesters", semesters)
+    super.editSetting(entity)
+  }
   
   override def saveAndRedirect(entity:Calendar):View = {
     val calendar = entity.asInstanceOf[CalendarBean]
