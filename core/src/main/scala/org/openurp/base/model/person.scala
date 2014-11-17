131686b8c4bdb5d31e664a01d3abc13dbf2c1330
xinzhou <xin.2165@gmail.com>
2014-11-17 13:29:41 +0800

    add nation in persong

--- core/src/main/scala/org/openurp/base/model/person.scala
+++ core/src/main/scala/org/openurp/base/model/person.scala
@@ -5,6 +5,7 @@ import org.beangle.data.model.bean.{ CodedBean, LongIdBean, NamedBean, UpdatedBe
 import org.openurp.base.{ Department, Person }
 import org.openurp.base.code.{ Country, Gender, IdType, PersonCategory }
 import org.beangle.data.model.bean.IntIdBean
+import org.openurp.base.code.Nation
 
 /**
  * 通用人员信息
@@ -17,6 +18,7 @@ class PersonBean extends LongIdBean with CodedBean with NamedBean with UpdatedBe
   var country: Country = _
   var department: Department = _
   var gender: Gender = _
+  var nation: Nation = _
   var category: PersonCategory = _
   var email: String = _
   var birthday: ju.Date = _
