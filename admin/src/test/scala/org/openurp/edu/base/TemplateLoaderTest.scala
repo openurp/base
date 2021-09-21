8b2b52d293271e68088e926246177cf56d661ee9
段体华 <duantihua@gmail.com>
2021-09-21 20:30:12 +0800

    update version 0.1.19

--- admin/src/test/scala/org/openurp/edu/base/TemplateLoaderTest.scala
+++ admin/src/test/scala/org/openurp/edu/base/TemplateLoaderTest.scala
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.openurp.base.edu
+package org.openurp.edu.base
 
 import org.beangle.commons.lang.ClassLoaders
 
